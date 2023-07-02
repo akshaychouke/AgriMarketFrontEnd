06aed5c425c4b32c5915f09eca6d524008432250
Akshay Chouke <akshaychauke50@gmail.com>
2023-07-02 13:19:14 +0530

    completed the routes and created file structure

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+const Contact = () => {
+  return (
+    <Layout>
+      <h1>Welcome to Contact</h1>
+    </Layout>
+  )
+}
+
+export default Contact
