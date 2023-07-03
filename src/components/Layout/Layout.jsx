e3f54d35fe1c63999a2cad2b1e7a77cfcc42df02
Akshay Chouke <akshaychauke50@gmail.com>
2023-07-03 10:59:56 +0530

    Added the basic SEO (search engine optimization) content

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,31 @@
 import React from "react";
-import Header from "./Header";
 import Footer from "./Footer";
-const Layout = ({ children }) => {
+import Header from "./Header";
+import { Helmet } from "react-helmet";
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
+      <Helmet>
+        {/* // Helmet is used to set meta tags for SEO purpose  */}
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
       <Header />
-      <main style={{ minHeight: "70vh" }}> {children}</main>
+      <main style={{ minHeight: "70vh" }}>{children}</main>
       <Footer />
     </div>
   );
+};
+
+// Default props for Layout component to avoid error
+Layout.defaultProps = {
+  title: "Ecommerce app - shop now",
+  description: "mern stack project for ecommerce app",
+  keywords: "mern,react,node,mongodb express ecommerce app ",
+  author: "Akshay Chouke",
 };
 
 export default Layout;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/Layout/Layout'
 const HomePage = () => {
   return (
-    <Layout>
+    <Layout title="Ecommerce App - Home">
       <h1>Welcome to HomePage</h1>
     </Layout>
   )
